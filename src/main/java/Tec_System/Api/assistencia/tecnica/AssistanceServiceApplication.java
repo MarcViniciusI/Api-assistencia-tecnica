548577c30064df87130cf88ec7a177b40e02703e
MarcViniciusI <marcosviniciusinacio@outlook.com>
2024-11-17 14:51:00 -0300

    Initial commit

---
+++ src/main/java/Tec_System/Api/assistencia/tecnica/AssistanceServiceApplication.java
@@ -0,0 +1,14 @@
+package Tec_System.Api.assistencia.tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal para iniciar a aplicação Spring Boot.
+ */
+@SpringBootApplication
+public class AssistanceServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AssistanceServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/Tec_System/Api/assistencia/tecnica/ApiAssistenciaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package Tec_System.Api.assistencia.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssistenciaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
